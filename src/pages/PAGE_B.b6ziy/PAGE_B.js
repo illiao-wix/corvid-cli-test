ce9be62be48048f8e019d05b8401db07a557c6b4
serhiiko <serhiiko@serhiiko-mbp15.local>
2020-01-03 14:49:40 +0200

    Add xmas image & text

--- src/pages/PAGE_B.b6ziy/PAGE_B.js
+++ src/pages/PAGE_B.b6ziy/PAGE_B.js
@@ -1,3 +1,4 @@
 $w.onReady(() => {
     console.log('New version of meh');
+    $w('#text2').text = 's max';
 })
--- src/pages/PAGE_B.b6ziy/pageElements.d.ts
+++ src/pages/PAGE_B.b6ziy/pageElements.d.ts
@@ -2,6 +2,8 @@ type PageElementsMap = {
   "#page1": $w.Page;
   "#text1": $w.Text;
   "#button1": $w.Button;
+  "#vectorImage1": $w.VectorImage;
+  "#text2": $w.Text;
   "#header1": $w.Header;
   "#footer1": $w.Footer;
 }
