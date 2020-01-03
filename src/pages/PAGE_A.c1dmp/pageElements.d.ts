d132ef7926a6ed1e8f81bff4f2d7aa12570f8ef6
illiao-wix <illiao@wix.com>
2020-01-03 14:43:45 +0200

    merged with design and developer code

--- src/pages/PAGE_A.c1dmp/pageElements.d.ts
+++ src/pages/PAGE_A.c1dmp/pageElements.d.ts
@@ -1,9 +1,10 @@
 type PageElementsMap = {
   "#page1": $w.Page;
+  "#text2": $w.Text;
   "#text1": $w.Text;
   "#button1": $w.Button;
   "#vectorImage1": $w.VectorImage;
-  "#text2": $w.Text;
+  "#vectorImage2": $w.VectorImage;
   "#header1": $w.Header;
   "#footer1": $w.Footer;
 }
