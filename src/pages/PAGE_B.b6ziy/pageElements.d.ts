58bd1ab48a302376717af77b2313b7c08c6fe193
illiao-wix <illiao@wix.com>
2020-01-03 13:31:10 +0200

    back to normal

--- src/pages/PAGE_B.b6ziy/pageElements.d.ts
+++ src/pages/PAGE_B.b6ziy/pageElements.d.ts
@@ -2,7 +2,6 @@ type PageElementsMap = {
   "#page1": $w.Page;
   "#text1": $w.Text;
   "#button1": $w.Button;
-  "#vectorImage1": $w.VectorImage;
   "#header1": $w.Header;
   "#footer1": $w.Footer;
 }
