b97160641146cf2eed371829961c246cdc0fa1f9
illiao-wix <illiao@wix.com>
2020-01-03 14:59:28 +0200

    pushed site

--- src/pages/PAGE_A.c1dmp/pageElements.d.ts
+++ src/pages/PAGE_A.c1dmp/pageElements.d.ts
@@ -3,9 +3,9 @@ type PageElementsMap = {
   "#text2": $w.Text;
   "#text1": $w.Text;
   "#button1": $w.Button;
+  "#button2": $w.Button;
   "#vectorImage1": $w.VectorImage;
   "#vectorImage2": $w.VectorImage;
-  "#button2": $w.Button;
   "#header1": $w.Header;
   "#footer1": $w.Footer;
 }
